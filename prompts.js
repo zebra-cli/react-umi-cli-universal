39a4d36165e35f8beded16bcf82c5174adf025d0
ReliaMM <tianyuan.mao@zebra-c.com>
2020-04-18 21:53:46 +0800

    feat: 修改 prompts

--- prompts.js
+++ prompts.js
@@ -7,12 +7,12 @@ module.exports = [
   {
     type: 'input',
     name: 'test_url',
-    message: '请输入测试服务器地址?'
+    message: '请输入测试服务器地址<http://example.com>?'
   },
   {
     type: 'input',
     name: 'test_ip',
-    message: '测试服务器IP?'
+    message: '测试服务器IP<127.0.0.1>?'
   },
   {
     type: 'input',
